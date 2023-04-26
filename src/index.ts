21835d098c96f961449369654a73ea184328ddea
Gritsyuk <ksundelife@yandex.ru>
2023-04-26 14:37:46 +0300

    [setting_4_1]: connected the react  and configured the css loader

--- src/index.ts
+++
@@ -1,7 +0,0 @@
-import { lof } from "./test";
-
-lof(0);
-
-const tempDiv = document.createElement("div");
-tempDiv.textContent = "Hello world 12222!";
-document.body.insertAdjacentElement("afterend", tempDiv);
