c9585f6549ea743228df6e54a6ff23fc2e16cc88
Gritsyuk <ksundelife@yandex.ru>
2023-04-30 13:50:13 +0300

    [setting_5_1]: css modules are configured

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,18 +1,30 @@
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import webpack from "webpack";
+import { BuildOptions } from "./types/config";
 
-export function buildLoaders(): webpack.RuleSetRule[] {
+export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
 
     const cssLoader = {
         test: /\.s[ac]ss$/i,
         use: [
-          // Creates `style` nodes from JS strings
-          "style-loader",
-          // Translates CSS into CommonJS
-          "css-loader",
-          // Compiles Sass to CSS
-          "sass-loader",
-        ],
-      };
+                // Creates `style` nodes from JS strings
+                isDev ? "style-loader" : MiniCssExtractPlugin.loader,
+                // Translates CSS into CommonJS
+                {
+                    loader: "css-loader",
+                    options: {
+                        modules: {
+                            auto: (resPath: string) => Boolean(resPath.includes(".module.")),
+                            localIdentName: isDev
+                                ? "[path][name]__[local]--[hash:base64:5]"
+                                : "[hash:base64:8]"
+                        }
+                    }
+                },
+                // Compiles Sass to CSS
+                "sass-loader",
+            ],
+        };
 
     // Если не используем typescript - нужен babel-loader
     const typescriptLoader = {
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Counter } from "./components/Counter";
+import "./index.sass";
+
+export const App = () => {
+
+    return (
+        <div className="app">
+            kjnfkjndkfjgn
+             <Counter />
+        </div>
+    );
+};
