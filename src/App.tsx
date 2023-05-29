a953b390a4f5264456847691e22d5d81afb8def4
Gritsyuk <ksundelife@yandex.ru>
2023-05-29 14:28:22 +0300

    [feature_7_3]: added dynamic toggle theme state

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
-import { Suspense } from "react";
+import { Suspense, useState } from "react";
 import { Link, Route, Routes } from "react-router-dom";
 import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
 import { MainPageAsync } from "./pages/MainPage/MainPage.async";
 import "./styles/index.sass";
 
+export enum Theme {
+    LIGHT = "light",
+    DARK = "dark"
+};
+
 const App = () => {
+    const [theme, setTheme] = useState<Theme>(Theme.LIGHT);
+
+    const toggleTheme = () => {
+        setTheme(theme === Theme.DARK ? Theme.LIGHT : Theme.DARK);
+    };
 
     return (
-        <div className="app light">
+        <div className={`app ${theme}`}>
+            <button onClick={toggleTheme}>TOGGLE</button>
             <Link to={"/"}>Main</Link>
             <Link to={"/about"}>About</Link>
             <Suspense fallback={<div>Loading...</div>}>
